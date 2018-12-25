282839e758c3148f4e28e39dfbac93959ead3e9f
cbat01 <2546173476@qq.com>
2018-12-25 19:39:36 +0800

    优化ajax函数即ajax2

--- js/ajax.js
+++ js/ajax.js
@@ -36,3 +36,58 @@ function myAjax(type,url,params,dataType,callback){
         }
     }
 }
+function myAjax2(obj){
+    var defautls = {
+        type:"get",
+        url:"#",
+        data:{},
+        dataType:"text",
+        success = function(result){console.log(result)}
+    }
+
+    for(var key in obj){
+        defaults[key] = obj[key];
+    }
+
+    var xhr = new XMLHttpRequest();
+
+    var params = "";
+    for(var attr in defaults.data){
+        params = params+attr+"="+defaults.data[attr]+"&";
+    }
+    if(params){
+        params = params.substring(0, params.length-1);
+    }
+
+    if(defaults.type == "get"){
+        defaults.url = defaults.url + "?" + params;
+    }
+
+    xhr.open(defaults.type,defautls.url,true);
+
+    if(defautls.type == "get"){
+        xhr.send(null);
+    }else if(defaults.type == "post"){
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(params);
+    }
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            if(xhr.status == 200){
+                var result = null;
+                if(defaults.dataType == "json"){
+                    result =xhr.responseText;
+                    result = JSON.parse(result);
+                }
+                else if(defaults.dataType == "xml"){
+                    result = xhr.responseText;
+                }else{
+                    result = xhr.responseText;
+                }
+
+                defaults.success(result);
+            }
+        }
+    }
+}
